7015302036a0872111ef57df5820ee6780a52980
Diana Toquica <94573450+dmtoquica@users.noreply.github.com>
2026-03-05 23:36:55 +0000

    Subiendo con error

--- utils/validador.js
+++ utils/validador.js
@@ -1,6 +1,6 @@
 // Esta función es nuestra lógica de negocio pura
 const esReservaValida = (roomId) => {
-    return typeof roomId === 'number' && roomId > -1;
+    return typeof roomId === 'number' && roomId > 9999;
 };
 
 module.exports = { esReservaValida };
